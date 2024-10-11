812cc79ac3a1bffc9ce7110502290b45494a14e2
Suraj-0007 <139526778+Suraj-0007@users.noreply.github.com>
2024-10-11 17:01:00 +0530

    Create Solution.java
    Given an array of integers, where all elements but one occur twice, find the unique element.
    
    Example
    
    The unique element is 4.
    
    Function Description:
    
    Complete the lonelyinteger function in the editor below.
    lonelyinteger has the following parameter(s):
    int a[n]: an array of integers
    
    Returns:
    int: the element that occurs only once
    
    Input Format:
    The first line contains a single integer, n, the number of integers in the array.
    The second line contains n space-separated integers that describe the values in a.

---
+++ LonelyInteger/Solution.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int lonelyinteger(List<Integer> a) {
+        int s = a.size();
+        int res = 0;
+        for(int i = 0; i < s; i++){
+            res ^= a.get(i);
+        }
+        return res;
+    }
+}
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.lonelyinteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
